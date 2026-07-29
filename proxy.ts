f25cdce6a73a16e8d6b86f76b327f259a00cc66a
rahulverma307 <rahulsi9810@gmail.com>
2026-07-29 09:34:38 +0530

    refactor: update clerk middleware to support regex path matching for auth routes

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,24 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(async (auth,req) => {
-    if(!isPublicRoutes(req)){
-        await auth.protect();
-    }
-})
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
-const isPublicRoutes = createRouteMatcher([
-    '/sign-in',
-    '/sign-up',
-    '/'
-])
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Skip Next.js internals and static files
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
-    // Always run for Clerk-specific frontend API routes
-    '/__clerk/(.*)',
+    "/(api|trpc)(.*)",
+    // Always run for Clerk frontend API routes
+    "/__clerk/(.*)",
   ],
-}
+};
