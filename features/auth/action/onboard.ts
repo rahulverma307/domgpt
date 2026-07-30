9ff93d21233494246edb747f2cd94e6dd117a62d
rahulverma307 <rahulsi9810@gmail.com>
2026-07-30 15:47:39 +0530

    feat: add server action to sync Clerk user data with database via upsert

--- features/auth/action/onboard.ts
+++ features/auth/action/onboard.ts
@@ -2,7 +2,6 @@
 
 import {currentUser} from "@clerk/nextjs/server";
 import { prisma } from "@/lib/db";
-import type {User} from "@/lib/generated/prisma/client";
 
 export async function onBoard(){
     const clerkUser = await currentUser();
