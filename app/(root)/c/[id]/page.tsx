917a663f928dc3abc3400e208851db7dd934e932
rahulverma307 <rahulsi9810@gmail.com>
2026-08-07 09:41:02 +0530

    feat: implement core chat interface components and conversation view routing

--- app/(root)/c/[id]/page.tsx
+++ app/(root)/c/[id]/page.tsx
@@ -1,32 +1,33 @@
-import { getConversation } from "@/features/conversation/actions/conversation.action";
-import { notFound } from "next/navigation";
-import { loadChatMessages } from "@/features/ai/actions/chat-store";
-import { ConversationView } from "@/features/conversation/components/conversion-view";
-import React from "react";
-
-
+import { loadChatMessages } from '@/features/ai/actions/chat-store';
+import { getConversation } from '@/features/conversation/actions/conversation.action';
+import { ConversationView } from '@/features/conversation/components/conversion-view';
+import { notFound } from 'next/navigation';
+import React from 'react'
 
 type ConversationPageProps = {
-  params:Promise<{id:string}>
-}
-
-const page = async({params}:ConversationPageProps)=>{
-  const {id} = await params;
-try{
-  await getConversation(id);
-}catch(error){
-notFound()
-} 
-
-const intialMessages = await loadChatMessages(id)
+    params: Promise<{ id: string }>;
+  };
 
 
-  return(
+const page = async({params}:ConversationPageProps) => {
+    const {id} = await params;
+
+    try {
+      await getConversation(id)
+    } catch (error) {
+      notFound()
+    }
+
+    const initialMessages = await loadChatMessages(id);
+    
+
+  return (
     <ConversationView
-    key={id}
-    initialMessages={initialMessages}
-    conversationId={id}
+      key={id}
+      conversationId={id}
+      initialMessages={initialMessages}
     />
   )
 }
+
 export default page
